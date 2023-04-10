3764e5a043124eb94760625aa9f561feab0d3d92
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2023-04-10 14:19:01 -0600

    Created the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import bossSlam from './images/boss.gif';
+import bossIdle from './images/bossIdling.gif';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+   const [posts, setPosts] = useState([]);
+   const [bossMan, setBossMan] = useState(bossIdle);
+   const [paperStyle, setPaperStyle] = useState("");
+   const [screenShake, setScreenShake] = useState("");
+
+   function rand(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+   useEffect(() => {
+      fetch('https://corporatebs-generator.sameerkumar.website/')
+         .then((response) => response.json())
+         .then((data) => {
+            setPosts(data);
+            console.log(data.phrase);
+         })
+         .catch((err) => {
+            console.log(err.message);
+         });
+   }, []);
+
+   function getQuote(){
+    fetch('https://corporatebs-generator.sameerkumar.website/')
+    .then((response) => response.json())
+    .then((data) => {
+       setPosts(data);
+       console.log(data.phrase);
+    })
+    .catch((err) => {
+       console.log(err.message);
+    });
+   }
+
+   function createPaper(){
+    setTimeout(()=>{
+      createPaper();
+    },rand(500,3000));
+   }
+
+   createPaper();
+return (
+  <>
+  <div className="graph">
+    <div className="bar"></div>
+  </div>
+  <div className="centerContainer">
+    <div className={screenShake+" centerContainer"}>
+    <p className="textBubble">I Need You to {posts.phrase}</p>
+    <div className="textBubbleArrow"></div>
     </div>
-  );
-}
+    <img alt="Large boss wearing a suit" className="boss" src={bossMan}/>
+    <div className="desk">
+    <div onClick={()=>{
+      setScreenShake("");
+      setBossMan(bossSlam);
+      setPaperStyle("falling");
+        setTimeout(()=>{
+            setScreenShake("screenShake");
+            getQuote();
+            setBossMan(bossIdle);
+            setPaperStyle("");
+        },2000);}}
+          className={paperStyle + " paper"}>
+      <div className="line"></div>
+      <div className="lineSmaller"></div>
+      <div className="line"></div>
+      <div className="lineSmaller"></div>
+      <div className="lineSmaller"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+    </div>
+      {/* <p className="rightArrow" style={{left:paperStyle+10+"%"}}>&rarr;</p> */}
+      <div className="paperBasket"></div>
+    </div>
+  </div>
+  </>
+);
+};
 
 export default App;
+
